e3f45cf9cd3b49fdf551ebe8418af9af509c980d
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-15 11:41:59 -0500

    added update and post

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
 import functions from "firebase-functions";
 import cors from "cors";
 import express from "express";
-import { getAllTasks } from "./src/tasks.js";
+import { getAllTasks, addTask, updateTask } from "./src/tasks.js";
+
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
 app.get('/tasks', getAllTasks)
-// app.post('/tasks', addTask)
-// app.patch('/tasks/:taskId', updateTask)
+app.post('/tasks', addTask)
+app.patch('/tasks/:taskId', updateTask)
 
 export const api = functions.https.onRequest(app)
--- functions/src/tasks.js
+++ functions/src/tasks.js
@@ -1,15 +1,30 @@
 import { getFirestoreInstance } from "./utils.js";
+import { FieldValue } from "firebase-admin/firestore";
 
 export function getAllTasks(req, res) {
     const db = getFirestoreInstance();
-    db.collection('tasks').get()
+    db.collection('tasks').sortBy('createdAt', 'desc').get()
     .then(collection => {
         const tasks = collection.docs.map(doc => ({ taskId: doc.Id, ...doc.data() }))
         res.send(tasks)
     })
-    .catch((err => res.status(500).json({error: err.message})))
+    .catch((err => res.status(500).json({error: err.message})));
 }
 
 export async function addTask(req, res) {
-    
+    const { task } = req.body
+    const newTask = { task, createdAt: FieldValue.serverTimestamp() }
+    const db = await getFirestoreInstance();
+    db.collection('tasks').add(newTask)
+    .then(() => getAllTasks(req, res))
+    .catch(err => res.status(500).send({ error: err.message }));
+}
+
+export async function updateTask(req, res) {
+    const { done } = req.body;
+    const { taskId } =req.params;
+    const db = await getFirestoreInstance();
+    db.collection('tasks').doc(taskId).update({ done })
+    .then(() => getAllTasks(req, res))
+    .catch(err => res.status(500).send({ error: err.message }));
 }
